95a09374ba488e7b59bc1a62acab04556d5239c7
Frank Calise <fcalise@gmail.com>
2016-09-01 13:58:16 -0400

    Initial commit

---
+++ xamarin-xf-nav-demo/xamarin_xf_nav_demo.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace xamarin_xf_nav_demo.Droid
+{
+    [Activity(Label = "xamarin_xf_nav_demo", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
