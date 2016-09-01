af7e88c5a19f6a89864409d7eb09e9419e22573a
Frank Calise <fcalise@gmail.com>
2016-09-01 19:50:40 -0400

    Implemented page switching

---
+++ xamarin-xf-nav-demo/xamarin_xf_nav_demo/HelpPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace xamarin_xf_nav_demo
+{
+    public class HelpPage : ContentPage
+    {
+        public HelpPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Help & feedback page" }
+                }
+            };
+        }
+    }
+}
---
+++ xamarin-xf-nav-demo/xamarin_xf_nav_demo/SettingsPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace xamarin_xf_nav_demo
+{
+    public class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Settings page" }
+                }
+            };
+        }
+    }
+}
